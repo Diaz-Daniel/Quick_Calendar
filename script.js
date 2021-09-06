75bffedae76194c85dfb7c0c0edf1c3ffa0edb85
DDiaz <daniel.diaz.0515@gmail.com>
2021-09-05 20:12:01 -0700

    Modified textinput color for css Started timeColor function for js Change class for textinput color Html

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+
+
+
+
 //Date and Time Display
 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 console.log(today);
+
+
+//Function that applies color change every hour
+
+var timeColor = function(){
+var H = moment().hour('H');
+console.log(H);
+
+}
