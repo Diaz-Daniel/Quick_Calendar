fa1ca3508712f763e02f3d03437a453fb060b85b
DDiaz <daniel.diaz.0515@gmail.com>
2021-09-11 08:08:49 -0700

    'changed js code to jquery'

--- script.js
+++ script.js
@@ -1,118 +1,37 @@
+$(".saveBtn").on("click", function(){
+    var text=  $(this).siblings('.description').val();
+    var hour = $(this).parent().attr('id');
 
-//Date and Time Display
-
-var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
-console.log(today);
-
-//variable and function for getting current hour
-var currentHour;
-var questionIndex = 0;
+    //need to save text and hour into localStorage
+    localStorage.setItem(hour, text);
 
 
-var timeColor = function(){
-currentHour = moment().hour();
-console.log(currentHour);
-}
-
-timeColor();
-
-//past present and future
-var pst = $('.past');
-var prsnt = $('.present');
-var futr = $('.future');
+});
 
 
-//assigning id of every input box to a letter
-var A = 9;
-var B = 10;
-var C = 11;
-var D = 12;
-var E = 13;
-var F = 14;
-var G = 15;
-var H = 16;
-var I = 17;
-
-var a = $('#9am');
-var b = $('#10am');
-var c = $('#11am');
-var d = $('#12pm');
-var e = $('#13pm');
-var f = $('#14pm');
-var g = $('#15pm');
-var h = $('#16pm');
-var i = $('#17pm');
-
-
-var hours =[
-    {
-        time: [A,B],
-        color: [a,b]
-    }
-
-]
-// console.log(hours.color[0]);
-
-function colorCheck(){
-var currentTextbox = hours[questionIndex];
-
-for (i= 0; i < currentTextbox.time.length; i++){
-
-        if (currentTextbox.time[i] < currentHour){
-            console.log("past");
-
-            
-        }else if(currentTextbox.time[i] > currentHour){
-            console.log('future')    
+function hourStyle(){
+    var returnHour = moment().hours();
+    console.log(returnHour);
+    
+    $('.time-block').each(function(){
+        var hourBlock = parseInt($(this).attr('id'))
+        console.log(hourBlock)
+        if(hourBlock < returnHour){
+            $(this).addClass("past");
+        }else if (returnHour === hourBlock){
+            $(this).removeClass("past")
+            $(this).addClass("present")
         }else{
-            console.log('present')
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+            
         }
-    }
+    })
 }
-colorCheck();
 
-//SAVE IN CASE YOU NEED TO START AGAIN
+hourStyle();
 
-// Variables for past, present, future
-// var pst = $('.past');
-// var prsnt = $('.present');
-// var futr = $('.future');
+$("#10 .description").val(localStorage.getItem(key));
 
-// var a = $('#9am');
-// var b = $('#10am');
-// var c = $('#11am');
-// var d = $('#12pm');
-// var e = $('#13pm');
-// var f = $('#14pm');
-// var g = $('#15pm');
-// var h = $('#16pm');
-// var i = $('#17pm');
-
-// //assing each letter a value
-// var A = 9;
-// var B = 10;
-// var C = 11;
-// var D = 12;
-// var E = 13;
-// var F = 14;
-// var G = 15;
-// var H = 16;
-// var I = 17;
-
-
-// //put variables in an array
-// var ibHours = [A,B];
-
-// //test function
-// for (i= 0; i < ibHours.length; i++){
-//     if (ibHours[i] < currentHour){
-//         console.log("past");
-//     }else if(ibHours[i] > currentHour){
-//         console.log('future')    
-//     }else{
-//         console.log('present')
-//     }
-// }
-// console.log(typeof ibHours[0]);
-// console.log(typeof currentHour);
+$('#currentDay').text(moment().format('MMM Do YYYY'));
